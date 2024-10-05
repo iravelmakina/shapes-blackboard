cd1b3265a1028ab8fd0fc023d7ea96f02f0f0a91
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-10-05 16:43:43 +0300

    drawCircle()
    - declared and defined drawCircle Bresenham algorithm to draw any circle using helper method plotCirclePoints()

--- BresenhamAlgorithm.cpp
+++ BresenhamAlgorithm.cpp
@@ -4,7 +4,7 @@
 
 
 namespace BresenhamAlgorithm {
-    void drawLine(std::vector<std::vector<char> > &grid, int  x1, int y1, const int x2, const int y2) {
+    void drawLine(std::vector<std::vector<char> > &grid, int x1, int y1, const int x2, const int y2) {
         const int dx = abs(x2 - x1);
         const int dy = abs(y2 - y1);
         const int sx = (x1 < x2) ? 1 : -1;
@@ -28,5 +28,38 @@ namespace BresenhamAlgorithm {
                 y1 += sy;
             }
         }
+    }
+
+
+    void drawCircle(std::vector<std::vector<char> > &grid, const int x_center, const int y_center, const int radius) {
+        int x = 0;
+        int y = radius;
+        int d = 3 - 2 * radius;
+
+        plotCirclePoints(grid, x_center, y_center, x, y);
+
+        while (x <= y) {
+            x++;
+            if (d > 0) {
+                y--;
+                d = d + 4 * (x - y) + 10;
+            } else {
+                d = d + 4 * x + 6;
+            }
+            plotCirclePoints(grid, x_center, y_center, x, y);
+        }
+    }
+
+
+    void plotCirclePoints(std::vector<std::vector<char> > &grid, const int x_center, const int y_center, const int x,
+                          const int y) {
+        if (x_center + x < grid[0].size() && y_center + y < grid.size()) grid[y_center + y][x_center + x] = '#';
+        if (x_center - x >= 0 && y_center + y < grid.size()) grid[y_center + y][x_center - x] = '#';
+        if (x_center + x < grid[0].size() && y_center - y >= 0) grid[y_center - y][x_center + x] = '#';
+        if (x_center - x >= 0 && y_center - y >= 0) grid[y_center - y][x_center - x] = '#';
+        if (x_center + y < grid[0].size() && y_center + x < grid.size()) grid[y_center + x][x_center + y] = '#';
+        if (x_center - y >= 0 && y_center + x < grid.size()) grid[y_center + x][x_center - y] = '#';
+        if (x_center + y < grid[0].size() && y_center - x >= 0) grid[y_center - x][x_center + y] = '#';
+        if (x_center - y >= 0 && y_center - x >= 0) grid[y_center - x][x_center - y] = '#';
     }
 }
--- BresenhamAlgorithm.h
+++ BresenhamAlgorithm.h
@@ -5,4 +5,8 @@
 
 namespace BresenhamAlgorithm {
     void drawLine(std::vector<std::vector<char> > &grid, int x1, int y1, int x2, int y2);
+
+    void drawCircle(std::vector<std::vector<char> > &grid, int x_center, int y_center, int radius);
+
+    void plotCirclePoints(std::vector<std::vector<char> > &grid, int x_center, int y_center, int x, int y);
 }
